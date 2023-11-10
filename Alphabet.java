414f7b02fcd0dee6c17153955ae556a6d29749d8
raichle001 <luca.raichle@t-online.de>
2023-11-10 14:37:13 -0500

    Renaming/Naming conventions done

--- Alphabet.java
+++ Alphabet.java
@@ -4,7 +4,6 @@ public class Alphabet {
 	public static final String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	public static final String LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
 	public static final String NUMBERS = "1234567890";
-
 	public static final String SYMBOLS = "!@#$%^&*()-_=+\\/~?";
 	
 	private final StringBuilder pool;
